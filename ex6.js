9f3550f9f8604e89629116e28988160c6b1a6f21
Jessica Pratt <55836289+Jessica-Pratt@users.noreply.github.com>
2019-09-26 20:36:17 +0930

    Add files via upload

---
+++ ex6.js
@@ -0,0 +1,6 @@
+var myName
+var myAge
+myName = prompt("Enter your name" , "name");
+myAge = prompt ("Enter your age" , "age");
+document.write ("Hello " + myName);
+document.write (", are you really " + myAge, " years old?");
